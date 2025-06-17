93035c1d475243abae2159080cfa0b7918dcf1fd
Faisal <muhammadfaisal95610@gmail.com>
2025-06-17 22:34:29 +0530

    DT:17/06/25

--- video119/src/App.jsx
+++ video119/src/App.jsx
@@ -1,25 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import {useForm} from 'react-hook-form'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const{ register, handleSubmit, formState: { errors, isSubmitting } } = useForm();
+  const{ register, handleSubmit, setError, formState: { errors, isSubmitting } } = useForm();
 
-  const onSubmit = (data) => {
+    const delay = (d) =>{
+        return new Promise((resolve, reject)=>{
+            setTimeout((d) =>{
+                resolve();
+            }, d)
+        })
+    }
+  const onSubmit = async (data) => {
+    await delay(2000); // Simulate a network request
     console.log(data);
+    if(data.username === "Admin"){
+        setError("myForm", { message:"Username Invalid"});
+    }
+    if(data.username === "Rohan"){
+        setError("blocked", { message:"Username already exists"});
+    }
   }
 
   return (
     <>
     {isSubmitting && <p>Submitting...</p>}
 <form action="" onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("username", { required: "Username is required" })} placeholder="Enter your username" /> <br /> <hr />
+      <input {...register("username", { required: "Username is required", minLength:{value: 5, message: "Username must be atleast 5 characters"}})} placeholder="Enter your username" /> <br /> <hr />
       {errors.username && <p>{errors.username.message} </p>}
-      <input {...register("password", { required: "Password is required" })} placeholder="Enter your password" /> <br />  <hr />
+      <input {...register("password", { required: "Password is required", maxLength:{value: 8, message: "Password must be atmost 8 characters"}})} placeholder="Enter your password" /> <br />  <hr />
       {errors.password && <p>{errors.password.message} </p>}
+
+      {errors.myForm && <p>{errors.myForm.message}</p>}
+      {errors.blocked && <p>{errors.blocked.message}</p>}
 
       <button disabled={isSubmitting} type='submit'>Submit</button>
 </form>
